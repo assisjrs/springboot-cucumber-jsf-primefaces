b2aab99d82cb3427179bbeecf64d49cc15a8e52e
DESKTOP-7IOG429\CSSP <gustavo.oliveira@concrete.com.br>
2017-03-06 15:05:55 -0300

    - alteração para funcionar a feature.

--- cucumber-app/src/test/java/stepDefinition/CadastroPage.java
+++ cucumber-app/src/test/java/stepDefinition/CadastroPage.java
@@ -4,33 +4,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by assis on 03/03/17.
  */
 public class CadastroPage {
+    public static final String ADDRESS = "http://localhost";
+    public static final String PORT = ":8083";
+    public static final String INDEX = "/index.xhtml";
+
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private String nome;
 
     public CadastroPage(){
-        System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
 
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 60000);
     }
 
     public void novoUsuario() throws InterruptedException {
-        driver.get("http://localhost:8080/index.xhtml");
+        driver.get(ADDRESS+PORT+INDEX);
 
-        final WebElement link = driver.findElement(By.linkText("Novo Usuário"));
 
+        WebElement link = driver.findElement(By.linkText("Novo Usuário"));
         link.click();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("usuarioForm")));
         final WebElement nomeInput = driver.findElement(By.className("usuario.nome"));
 
         nomeInput.sendKeys(nome);
 
-        nomeInput.submit();
+        WebElement salvar = driver.findElement(By.id("usuarioForm:salvar"));
+
+        salvar.click();
+        //nomeInput.submit();
     }
 
     public String corpo(){
