d830df9b52fd52da5808f9d1d28d533820877fe5
“Assis <francisco.melo@concrete.com.br>
2017-03-07 19:38:26 -0300

    rollback

--- cucumber-app/src/test/java/database/Config.java
+++ cucumber-app/src/test/java/database/Config.java
@@ -1,8 +1,5 @@
 package database;
 
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.dataset.xml.XmlDataSet;
-import org.dbunit.operation.DatabaseOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +9,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Created by assis on 06/03/17.
@@ -20,13 +18,12 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @ComponentScan({"database.*", "stepDefinition.*"})
 public class Config {
-
     @Bean(name = "dbUnitDatabaseConnection")
-    public DataSource dbUnitDatabaseConnection() {
+    public DataSource dbUnitDatabaseConnection() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/leiloes?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://localhost/leiloes?nullCatalogMeansCurrent=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false");
         dataSource.setUsername("root");
         dataSource.setPassword("cs!rede123");
 
@@ -34,42 +31,7 @@ public class Config {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(){
+    public PlatformTransactionManager transactionManager() throws SQLException {
         return new DataSourceTransactionManager(dbUnitDatabaseConnection());
     }
-
-    /*
-    private static DataSource ds;
-    private static DatabaseConnection databaseConnection;
-
-    public static void createDataset(String dataset) {
-        if (!dataset.startsWith("/")) {
-            dataset = "/" + dataset;
-        }
-        try {
-            initConn();
-            DatabaseOperation.CLEAN_INSERT.execute(databaseConnection, new XmlDataSet(DBUnitUtils.class.getResourceAsStream(dataset)));
-        } catch (Exception e) {
-            throw new RuntimeException("could not initialize dataset:" + dataset +
-                    " \nmessage: " + e.getMessage());
-        } finally {
-            closeConn();
-        }
-    }
-
-    public static void deleteDataset(String dataset) {
-        if (!dataset.startsWith("/")) {
-            dataset = "/" + dataset;
-        }
-        try {
-            initConn();
-            DatabaseOperation.DELETE_ALL.execute(databaseConnection, new XmlDataSet(DBUnitUtils.class.getResourceAsStream(dataset)));
-        } catch (Exception e) {
-            throw new RuntimeException("could not delete dataset dataset:" + dataset +
-                    " \nmessage: " + e.getMessage());
-        } finally {
-            closeConn();
-        }
-    }
-    */
 }
--- cucumber-app/src/test/java/stepDefinition/CadastrarUmUsuarioStep.java
+++ cucumber-app/src/test/java/stepDefinition/CadastrarUmUsuarioStep.java
@@ -1,6 +1,6 @@
 package stepDefinition;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -8,13 +8,24 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import database.Config;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,19 +36,42 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(classes = { Config.class })
 @Transactional
 @TestExecutionListeners(value = { DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
-        DbUnitTestExecutionListener.class }, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
-@DatabaseSetup("./CadastrarUmUsuarioStep.xml")
+        TransactionDbUnitTestExecutionListener.class },
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@DatabaseSetup("CadastrarUmUsuarioStep.xml")
 public class CadastrarUmUsuarioStep {
     private CadastroPage cadastroPage;
 
+    @Autowired
+    private DataSource dbUnitDatabaseConnection;
+
     @Before
-    public void initWebdriver() {
+    public void initWebdriver() throws SQLException, DatabaseUnitException, IOException {
+        final FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        builder.setColumnSensing(true);
+
+        final IDataSet dataSet = builder.build(new File("src/test/resources/CadastrarUmUsuarioStep.xml"));
+        final DatabaseConnection databaseConnection = new DatabaseConnection(dbUnitDatabaseConnection.getConnection());
+
+        //DatabaseOperation.CLEAN_INSERT.execute(databaseConnection, dataSet);
+
         cadastroPage = new CadastroPage();
     }
 
     @After
     public void destroyWebdriver() throws Throwable {
         cadastroPage.finalize();
+
+        final FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        builder.setColumnSensing(true);
+
+        final IDataSet dataSet = builder.build(new File("src/test/resources/CadastrarUmUsuarioStep.xml"));
+        final DatabaseConnection databaseConnection = new DatabaseConnection(dbUnitDatabaseConnection.getConnection());
+
+        dbUnitDatabaseConnection.getConnection().setAutoCommit(false);
+        dbUnitDatabaseConnection.getConnection().rollback();
+
+        //DatabaseOperation.DELETE_ALL.execute(databaseConnection, dataSet);
     }
 
     @Given("^o nome do usuario as \"([^\"]*)\" e o email \"([^\"]*)\"$")
